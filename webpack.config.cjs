2a5e855823df756eb4db900f7ec9b0051d8665f2
itsdankai <daniel.kaizer@icloud.com>
2025-03-18 11:05:45 -0500

    Remove old Webpack config file

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,10 +1,6 @@
-import path from "path";
-import { fileURLToPath } from "url";
+const path = require("path");
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export default {
+module.exports = {
   entry: "./public/main.js",
   output: {
     filename: "bundle.js",
